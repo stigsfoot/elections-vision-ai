5e6c6ed021d1ae2795dde80e40e9dbe1fc59e317
Noble Ackerson <noble.ackerson@gmail.com>
2018-03-05 09:07:52 -0500

    feat: Stage application for Cloud functions

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Setup work
+
+// Cloud functions declarations go here
+
+// Publishes result to a given topic and returns a Promise
+function publishResult(){}
+// Detects the text in an image using Google Vision API
+function detectText(){}
+// Sends some function call to Twilio or similar service
+function uploadImage(data) {return data;}
+//Captures the image from User camera.
+function captureImage(){}
+//Reads DialogFlow intent that will trigger image capture/upload
+function readIntent(){}
