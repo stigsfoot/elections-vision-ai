989227b3018b509fcddb907e4cc38f7221a2c226
Noble Ackerson <noble.ackerson@gmail.com>
2018-03-07 22:30:16 -0500

    feat: declarations and capture image logic

--- README.md
+++ README.md
@@ -1,5 +1,43 @@
 # NDITECH Elections Vision Ai
 Demonstration on how to use computer vision and bots to automate the collection of election data
 
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Support](#support)
+- [Contributing](#contributing)
+
+## Installation
+
+Clone this repo:
+
+```sh
+curl -LO http://git.io/Xy0Chg
+git add README.md
+git commit -m "Use README Boilerplate"
+```
+
+## Usage
+
+Replace the contents of `README.md` with your project's:
+
+- Name
+- Description
+- Installation instructions
+- Usage instructions
+- Support instructions
+- Contributing instructions
+
+Feel free to remove any sections that aren't applicable to your project.
+
+## Support
+
+Please [open an issue](../issues/new) for support.
+
+## Contributing
+
+Please contribute using [Github Flow](https://guides.github.com/introduction/flow/). Create a branch, add commits, and [open a pull request](https://github.com/fraction/readme-boilerplate/compare/).
+
 # License
 The contents of this repository are covered under the [MIT License](../master/LICENSE)
--- index.js
+++ index.js
@@ -4,6 +4,23 @@
 
 // Cloud functions declarations go here
 
+process.env.DEBUG = '*';
+
+const fs = require('fs');
+const child_process = require('child_process');
+const Buffer = require('safe-buffer').Buffer;
+
+const config = require('./config.json');
+
+const pubsub = require('@google-cloud/pubsub')();
+const storage = require('@google-cloud/storage')();
+const vision = require('@google-cloud/vision')();
+const translate = require('@google-cloud/translate')();
+
+const aog = require('actions-on-google');
+const DialogflowApp = aog.DialogflowApp;
+
+
 // Publishes result to a given topic and returns a Promise
 function publishResult(){}
 // Detects the text in an image using Google Vision API
@@ -11,6 +28,24 @@ function detectText(){}
 // Sends some function call to Twilio or similar service
 function uploadImage(data) {return data;}
 //Captures the image from User camera.
-function captureImage(){}
+const fs = require('fs');
+const child_process = require('child_process');
+const Buffer = require('safe-buffer').Buffer;
+/**
+ * Capture the image from the user computer's camera.
+ */
+function captureImage() {
+  return new Promise((res, rej) => {
+    const file = `/tmp/google-actions-reader-${Date.now()}.png`;
+    try {
+      child_process.execSync(`imagesnap -w 1 ${file}`);
+      const bitmap = fs.readFileSync(file);
+      res({
+        base64: new Buffer(bitmap).toString('base64'),
+        file
+      });
+    } catch (err) { rej(err); }
+  });
+}
 //Reads DialogFlow intent that will trigger image capture/upload
 function readIntent(){}
